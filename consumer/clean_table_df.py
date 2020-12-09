a73315af220281d405f644806d87f0b1e6d6f02e
JiuweiW <jiuweitongxue@gmail.com>
2020-12-09 14:13:12 -0500

    change date to object

--- consumer/clean_table_df.py
+++ consumer/clean_table_df.py
@@ -5,22 +5,20 @@ from pyspark.sql import SparkSession, functions, types
 cassandra_host = 'localhost'
 
 # add more functions as necessary
-def output_line(rdd):
-    orderkey, price, names = rdd
-    namestr = ', '.join(sorted(list(names)))
-    return 'Order #%d $%.2f: %s' % (orderkey, price, namestr)
 
-def main(date):
+def main(date_time_str):
     # main logic starts here
+    date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d')
+    
     emails_df = spark.read.format("org.apache.spark.sql.cassandra").options(table="emails", keyspace="email_database").load()
    
-    clean_df = emails_df.filter(emails_df['current_date'] > date)
+    clean_df = emails_df.filter(emails_df['current_date'] > date_time_obj.date())
     clean_df.write.format("org.apache.spark.sql.cassandra").options(table="emails", keyspace="email_database").save()
     
 
 if __name__ == '__main__':  
     #input like 2020-12-08
-    date = sys.argv[1]
+    date_time_str = sys.argv[1]
     spark = SparkSession.builder.appName('clean table df').config('spark.cassandra.connection.host', cassandra_host).getOrCreate()
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
