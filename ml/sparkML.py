4b020b1a5c31c034ca1c443721ed8cff4b9e3eb1
Shih-Chieh Chen <chen.j.shihchieh@gmail.com>
2020-12-09 12:07:04 -0800

    newest update

---
+++ ml/sparkML.py
@@ -0,0 +1,34 @@
+import sys
+from pyspark.sql import SparkSession, functions as f
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import HashingTF
+from pyspark.ml.classification import LinearSVC
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+cassandra_host = 'localhost'
+
+
+def main():
+    #raw = spark.read.format("org.apache.spark.sql.cassandra") \
+	#	.options(table="emails", keyspace"email_database").load()
+    raw = spark.read.json("DontCommit/MLsamples")
+    hashingtransformer = HashingTF(inputCol='word_tokens', outputCol='features')
+    hashingtransformer.setNumFeatures(5000)
+    svc = LinearSVC(featuresCol='features', labelCol='label', predictionCol='prediction')
+    svc_pipeline = Pipeline(stages=[hashingtransformer, svc])
+    evaluator = BinaryClassificationEvaluator(labelCol='label', rawPredictionCol='rawPrediction', metricName='areaUnderROC')
+    
+    train, validation = raw.randomSplit([0.75, 0.25])
+    train = train.cache()
+    validation = validation.cache()
+    svc_model = svc_pipeline.fit(train)
+    prediction = svc_model.transform(validation)
+    accuracy = evaluator.evaluate(prediction)
+    print('Validation score: %g' % (accuracy, ))
+
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName('emailml')\
+        .config('spark.cassandra.connection.host', cassandra_host).getOrCreate()
+    spark.sparkContext.setLogLevel('warn')
+    main()
